3e33a09a8472d3b62352a2087943f5be3da0c13a
Bruce <recall4056@gmail.com>
2023-03-08 02:51:23 +0800

    chore: add antd and dayjs

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,11 +1,14 @@
-export default function App() {
+import { Spin } from "antd"
+
+const App = (): JSX.Element => {
   const NOTICE_TEXT = "正在开发中..."
-  const EXPECTED_DATE_TEXT = "预期上线时间 - xxxx年xx月xx日"
+  const EXPECTED_DATE_TEXT = "预期上线时间 - 2023 年 xx 月 xx 日"
 
   return (
     <div className="w-screen h-screen">
       <div className="flex justify-center items-center h-full">
         <div className="flex flex-col text-center gap-4">
+          <Spin size="large" />
           <span>{NOTICE_TEXT}</span>
           <span className="text-sm">{EXPECTED_DATE_TEXT}</span>
         </div>
@@ -13,3 +16,5 @@ export default function App() {
     </div>
   )
 }
+
+export default App
--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+import { ConfigProvider } from "antd"
+import zhCN from "antd/locale/zh_CN"
+import dayjs from "dayjs"
+import "dayjs/locale/zh-cn"
+import "antd/dist/reset.css"
 import "./index.css"
+import App from "./App"
+
+dayjs.locale("zh-cn")
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 )
