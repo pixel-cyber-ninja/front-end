e2d17a45508d0995ef76dcb8234c158632edecec
Bruce <recall4056@gmail.com>
2023-03-08 02:38:52 +0800

    chore: add tailwindcss support

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const NOTICE_TEXT = "正在开发中..."
+  const EXPECTED_DATE_TEXT = "预期上线时间 - xxxx年xx月xx日"
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen">
+      <div className="flex justify-center items-center h-full">
+        <div className="flex flex-col text-center gap-4">
+          <span>{NOTICE_TEXT}</span>
+          <span className="text-sm">{EXPECTED_DATE_TEXT}</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
-
-export default App
